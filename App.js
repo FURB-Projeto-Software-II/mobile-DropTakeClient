c323299dd14bd05fd345956c9ee64050ac5dbcdc
Bruno <brunovigentas@gmail.com>
2020-04-28 20:09:48 -0300

    Commit inicial com redux e ui kitten configurados

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
+
+import reducers from './reducers'
+
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
+
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers)
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <Provider store={store}>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <HomeScreen/>
+      </ApplicationProvider>
+    </Provider>
+  )
+}
