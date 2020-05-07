4c3c7e4b54116fba3d050ce121b711df0d87608f
Bruno <brunovigentas@gmail.com>
2020-05-07 15:34:34 -0300

    Created homepage

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
-import { Layout, Text, Divider, Button } from '@ui-kitten/components';
+import MapView from 'react-native-maps';
+import { Layout, Text, Divider, Button, Card, View } from '@ui-kitten/components';
 import { bindActionCreators } from 'redux';
 import { Actions } from 'react-native-router-flux'
 
@@ -11,15 +12,34 @@ class Home extends Component {
         return (
             <>
                 <Layout style={styles.containerUp}>
+                    <MapView style={styles.mapStyle} >
                     <Button style={styles.button} onPress={() => Actions.orderCrud()}>
-                        NOVO PEDIDO
+                        <Text style={styles.buttonText}>NOVO PEDIDO</Text>
                     </Button>
+                    </MapView>
                 </Layout>
                 <Layout style={styles.containerDown}>
                     <Layout style={styles.welcomebox}>
-                        <Text style={styles.welcometext}>Bem-vindo, Bruno!</Text>
+                        <Text style={styles.welcometext}>Seus últimos pedidos, Bruno.</Text>
                     </Layout>
                     <Divider style={styles.divider}/>
+                    <Layout style={styles.orderList}>
+                        <Card style={styles.orderCard}>
+                            <Text category="label" style={styles.orderCardTitle}>TV Samgung</Text>
+                            <Text>Entrega para José Affonso - Rua Arthur Schreiber, 71 - Velha, Blumenau - SC</Text>
+                            <Text style={styles.orderCardStatus}>Pronto para retirada</Text>
+                        </Card>
+                        <Card style={styles.orderCard}>
+                            <Text category="label" style={styles.orderCardTitle}>TV Samgung</Text>
+                            <Text>Entrega para José Affonso - Rua Arthur Schreiber, 71 - Velha, Blumenau - SC</Text>
+                            <Text style={styles.orderCardStatus}>Pronto para retirada</Text>
+                        </Card>
+                    </Layout>
+                    <Layout style={styles.buttonBottom}>
+                        <Button style={styles.buttonOrderList} onPress={() => Actions.ordersList()}>
+                            <Text style={styles.buttonOrderListText}>VER TODOS PEDIDOS</Text>
+                        </Button>
+                    </Layout>
                 </Layout>
             </>
         )
@@ -61,4 +81,36 @@ const styles = StyleSheet.create({
         backgroundColor: '#501380',
         borderColor: 'transparent'
     },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    buttonOrderList: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent'
+    },
+    buttonOrderListText: {
+        color: '#501380'
+    },
+    buttonBottom: {
+        
+    },
+    orderList: {
+        height: '73%',
+    },
+    orderCard: {
+        margin: 15
+    },
+    orderCardTitle: {
+        marginBottom: 5
+    },
+    orderCardStatus: {
+        marginTop: 3,
+        fontWeight: 'bold',
+        color: 'green'
+    },
+    mapStyle: {
+        width: '100%',
+        height: '100%',
+      },
 })
--- pages/order-find-storage/order-find-storage.js
+++ pages/order-find-storage/order-find-storage.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import { Router, Scene } from 'react-native-router-flux'
-import { Text, StyleSheet } from 'react-native'
 
 import StorageMap from '../order-storage-map/order-storage-map'
 import StorageList from '../order-storage-list/order-storage-list'
+import { FontAwesome } from 'expo-vector-icons'
 
-const TabIcon = ({selected, title}) => {
+const MapIcon = ({selected, title}) => {
     return (
-        <Text style={{color: selected ? 'red' : 'black'}}>{title}</Text>
+        <FontAwesome name="map-o" size="20" color={selected ? "red" : "black"}/>
+    )
+}
+
+const ListIcon = ({selected, title}) => {
+    return (
+        <FontAwesome name="list" size="20" color={selected ? "red" : "black"}/>
     )
 }
 
@@ -20,14 +26,14 @@ class OrderFindStorage extends Component {
             <Router>
                 <Scene key="root" hideNavBar="true">
                     <Scene key="tabbar" tabs tabBarStyle={{backgroundColor: '#FFF'}} hideNavBar="true">
-                        <Scene key="map-tab" title="Map" icon={TabIcon}>
+                        <Scene key="map-tab" title="Map" icon={MapIcon}>
                             <Scene 
                                 key="map"
                                 component={StorageMap}
                                 hideNavBar="true"
                             />
                         </Scene>
-                        <Scene key="list-tab" title="List" icon={TabIcon} >
+                        <Scene key="list-tab" title="List" icon={ListIcon} >
                             <Scene 
                                 key="list"
                                 component={StorageList}
