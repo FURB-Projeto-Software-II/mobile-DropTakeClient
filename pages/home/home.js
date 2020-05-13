ec5f313a46407fdbf7876939d3f3a82adde55cbc
josehteixeira <joseehteixeira@gmail.com>
2020-05-12 21:32:55 -0300

    Buscar localização do usuário

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import { StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import MapView from 'react-native-maps';
@@ -6,57 +6,93 @@ import { Layout, Text, Divider, Button, Card, View } from '@ui-kitten/components
 import { bindActionCreators } from 'redux';
 import { Actions } from 'react-native-router-flux'
 import { FontAwesome } from 'expo-vector-icons'
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
-class Home extends Component {
+const Home = () => {
+    const [currentRegion, setCurrentRegion] = useState(null);
 
-    render() {
-        return (
-            <>
-                <Layout style={styles.containerUp}>
-                    <MapView style={styles.mapStyle} >
-                        <Button style={styles.button} onPress={() => Actions.orderCrud()}>
-                            <Text style={styles.buttonText}>NOVO PEDIDO</Text>
-                        </Button>
+    // Executa ao abrir o app para buscar a localizacao
+    useEffect(() => {
 
-                        <Button style={styles.configButton} onPress={() => Actions.configs()}>
-                            <FontAwesome name="gear" size={21} color="black" />
-                        </Button>
-                    </MapView>
-                </Layout>
-                <Layout style={styles.containerDown}>
-                    <Layout style={styles.welcomebox}>
-                        <Text style={styles.welcometext}>Seus últimos pedidos, Bruno.</Text>
-                    </Layout>
-                    <Divider style={styles.divider}/>
-                    <Layout style={styles.orderList}>
-                        <Card style={styles.orderCard} onPress={() => Actions.orderInfo()}>
-                            <Text category="label" style={styles.orderCardTitle}>TV Samgung</Text>
-                            <Text>Entrega para José Affonso - Rua Arthur Schreiber, 71 - Velha, Blumenau - SC</Text>
-                            <Text style={styles.orderCardStatus}>Pronto para retirada</Text>
-                        </Card>
-                        <Card style={styles.orderCard} onPress={() => Actions.orderInfo()}>
-                            <Text category="label" style={styles.orderCardTitle}>TV Samgung</Text>
-                            <Text>Entrega para José Affonso - Rua Arthur Schreiber, 71 - Velha, Blumenau - SC</Text>
-                            <Text style={styles.orderCardStatus}>Pronto para retirada</Text>
-                        </Card>
-                    </Layout>
-                    <Layout style={styles.buttonBottom}>
-                        <Button style={styles.buttonOrderList} onPress={() => Actions.ordersList()}>
-                            <Text style={styles.buttonOrderListText}>VER TODOS PEDIDOS</Text>
-                        </Button>
-                    </Layout>
-                </Layout>
-            </>
-        )
+        async function loadInitialPosition() {
+
+            // Requisita permissao para acessar localizacao do usuario
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+
+                // Busca a localizacao
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+
+                // Salva a localizacao atual
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                });
+            }
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    // Evento para atualizar o valor da posicao atual quando o usuario mexer no mapa
+    function handleRegionChanged(region) {
+        setCurrentRegion(region);
     }
 
+    console.log(JSON.stringify(currentRegion));
+
+    return (
+        <>
+            <Layout style={styles.containerUp}>
+                <MapView onRegionChangeComplete={handleRegionChanged} style={styles.mapStyle} initialRegion={currentRegion}>
+                    <Button style={styles.button} onPress={() => Actions.orderCrud()}>
+                        <Text style={styles.buttonText}>NOVO PEDIDO</Text>
+                    </Button>
+
+                    <Button style={styles.configButton} onPress={() => Actions.configs()}>
+                        <FontAwesome name="gear" size={21} color="black" />
+                    </Button>
+                </MapView>
+            </Layout>
+            <Layout style={styles.containerDown}>
+                <Layout style={styles.welcomebox}>
+                    <Text style={styles.welcometext}>Seus últimos pedidos, Bruno.</Text>
+                </Layout>
+                <Divider style={styles.divider} />
+                <Layout style={styles.orderList}>
+                    <Card style={styles.orderCard} onPress={() => Actions.orderInfo()}>
+                        <Text category="label" style={styles.orderCardTitle}>TV Samgung</Text>
+                        <Text>Entrega para José Affonso - Rua Arthur Schreiber, 71 - Velha, Blumenau - SC</Text>
+                        <Text style={styles.orderCardStatus}>Pronto para retirada</Text>
+                    </Card>
+                    <Card style={styles.orderCard} onPress={() => Actions.orderInfo()}>
+                        <Text category="label" style={styles.orderCardTitle}>TV Samgung</Text>
+                        <Text>Entrega para José Affonso - Rua Arthur Schreiber, 71 - Velha, Blumenau - SC</Text>
+                        <Text style={styles.orderCardStatus}>Pronto para retirada</Text>
+                    </Card>
+                </Layout>
+                <Layout style={styles.buttonBottom}>
+                    <Button style={styles.buttonOrderList} onPress={() => Actions.ordersList()}>
+                        <Text style={styles.buttonOrderListText}>VER TODOS PEDIDOS</Text>
+                    </Button>
+                </Layout>
+            </Layout>
+        </>
+    )
 }
 
 const mapStateToProps = state => ({
-    
+
 })
 
-const mapDispatchToProps = dispatch => bindActionCreators({ }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
 
@@ -73,7 +109,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 15,
         fontSize: 20,
-        
+
     },
     welcomebox: {
         alignItems: 'center',
@@ -98,7 +134,7 @@ const styles = StyleSheet.create({
         color: '#501380'
     },
     buttonBottom: {
-        
+
     },
     orderList: {
         height: '73%',
